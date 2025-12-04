8b8ee9b64072e9cffd67c122be7559ead57c3e65
Petar <petarsudar2@gmail.com>
2025-12-04 01:08:50 +0100

    Korisnik drop menu

--- BookVerse/Market.Frontend/client/src/app/components/header/header.component.ts
+++ BookVerse/Market.Frontend/client/src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -7,4 +7,18 @@ import { RouterLink } from '@angular/router';
   templateUrl: './header.component.html',
   styleUrl: 'header.component.css',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  @ViewChild('dropMenu') dropMenu!: ElementRef;
+
+  dropMenuOpened = false;
+
+  showDropMenu() {
+    console.log(this.dropMenu);
+    if (!this.dropMenuOpened) {
+      this.dropMenu.nativeElement.style.height = '455px';
+    } else {
+      this.dropMenu.nativeElement.style.height = '0px';
+    }
+    this.dropMenuOpened = !this.dropMenuOpened;
+  }
+}
