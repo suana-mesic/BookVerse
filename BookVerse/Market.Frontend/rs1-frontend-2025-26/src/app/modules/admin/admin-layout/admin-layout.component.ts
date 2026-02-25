ca4690a3777ccf5abce1e4f6776de884fb980ee4
Suana Mešić <suana.mesic@edu.fit.ba>
2026-02-25 21:40:00 +0100

    Add drag and drop functionality for sidebar sections with order persistence

--- BookVerse/Market.Frontend/rs1-frontend-2025-26/src/app/modules/admin/admin-layout/admin-layout.component.ts
+++ BookVerse/Market.Frontend/rs1-frontend-2025-26/src/app/modules/admin/admin-layout/admin-layout.component.ts
@@ -1,6 +1,7 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { AuthFacadeService } from '../../../core/services/auth/auth-facade.service';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-admin-layout',
@@ -8,7 +9,40 @@ import { AuthFacadeService } from '../../../core/services/auth/auth-facade.servi
   templateUrl: './admin-layout.component.html',
   styleUrl: './admin-layout.component.scss'
 })
-export class AdminLayoutComponent {
+export class AdminLayoutComponent implements OnInit{
+  navSections = [
+  {
+    id:'catalog',
+    label:'ADMIN_PANEL.MENU.CATALOG',
+    items:[
+      {route: '/admin/product-categories',icon:'category', label:'ADMIN_PANEL.MENU.CATEGORIES'},
+      {route: '/admin/products',icon:'inventory_2', label:'ADMIN_PANEL.MENU.BOOKS'},
+    ]
+  },
+  {
+    id:'orders',
+    label:'ADMIN_PANEL.MENU.SECTION',
+    items:[
+      {route: '/admin/orders',icon:'shopping_cart', label:'ADMIN_PANEL.MENU.ORDERS'},
+    ]
+  },
+    {
+    id:'system',
+    label:'ADMIN_PANEL.MENU.SYSTEM',
+    items:[
+      {route: '/admin/settings',icon:'settings', label:'ADMIN_PANEL.MENU.SETTINGS'},
+    ]
+  },
+];
+
+  ngOnInit(): void {
+    const saved = localStorage.getItem('navSectionsOrder');
+    if(saved){
+      const order = JSON.parse(saved);
+      this.navSections.sort((a,b)=>order.indexOf(a.id)-order.indexOf(b.id));
+    }
+  }
+
   private translate = inject(TranslateService);
   auth = inject(AuthFacadeService);
 
@@ -23,6 +57,7 @@ export class AdminLayoutComponent {
     this.currentLang = this.translate.currentLang || 'bs';
   }
 
+
   switchLanguage(langCode: string): void {
     this.currentLang = langCode;
     this.translate.use(langCode);
@@ -31,5 +66,10 @@ export class AdminLayoutComponent {
 
   getCurrentLanguage() {
     return this.languages.find(lang => lang.code === this.currentLang);
+  }
+
+  onSectionDrop(event: CdkDragDrop<any[]>){
+    moveItemInArray(this.navSections, event.previousIndex,event.currentIndex);
+    localStorage.setItem('navSectionsOrder',JSON.stringify(this.navSections.map(s=>s.id)))
   }
 }
